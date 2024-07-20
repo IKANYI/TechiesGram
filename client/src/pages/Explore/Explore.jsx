7550e2138791fb8406287216e6a8d75f10631456
IKANYI <paulkaranja214@gmail.com>
2024-07-20 08:13:47 +0300

    create the explore page

--- client/src/pages/Explore/Explore.jsx
+++ client/src/pages/Explore/Explore.jsx
@@ -1,7 +1,103 @@
 import React from "react";
+import proimg from "../../assets/profile.jpg";
+import blogimg from "../../assets/blogimg.jpg";
 
 function Explore() {
-  return <div>Explore</div>;
+  return (
+    <div className="explore-main">
+      <div className="explore-container">
+        <div className="user-info">
+          <img src={proimg} alt="" />
+          <h2>Paul Karanja</h2>
+        </div>
+        <div className="user-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            aspernatur quisquam eaque dolorem?
+          </p>
+          <img src={blogimg} alt="blog picture" />
+        </div>
+      </div>
+      <div className="explore-container">
+        <div className="user-info">
+          <img src={proimg} alt="" />
+          <h2>Paul Karanja</h2>
+        </div>
+        <div className="user-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            aspernatur quisquam eaque dolorem?
+          </p>
+          <img src={blogimg} alt="blog picture" />
+        </div>
+      </div>
+      <div className="explore-container">
+        <div className="user-info">
+          <img src={proimg} alt="" />
+          <h2>Paul Karanja</h2>
+        </div>
+        <div className="user-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            aspernatur quisquam eaque dolorem?
+          </p>
+          <img src={blogimg} alt="blog picture" />
+        </div>
+      </div>
+      <div className="explore-container">
+        <div className="user-info">
+          <img src={proimg} alt="" />
+          <h2>Paul Karanja</h2>
+        </div>
+        <div className="user-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            aspernatur quisquam eaque dolorem?
+          </p>
+          <img src={blogimg} alt="blog picture" />
+        </div>
+      </div>
+      <div className="explore-container">
+        <div className="user-info">
+          <img src={proimg} alt="" />
+          <h2>Paul Karanja</h2>
+        </div>
+        <div className="user-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            aspernatur quisquam eaque dolorem?
+          </p>
+          <img src={blogimg} alt="blog picture" />
+        </div>
+      </div>
+      <div className="explore-container">
+        <div className="user-info">
+          <img src={proimg} alt="" />
+          <h2>Paul Karanja</h2>
+        </div>
+        <div className="user-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            aspernatur quisquam eaque dolorem?
+          </p>
+          <img src={blogimg} alt="blog picture" />
+        </div>
+      </div>
+      <div className="explore-container">
+        <div className="user-info">
+          <img src={proimg} alt="" />
+          <h2>Paul Karanja</h2>
+        </div>
+        <div className="user-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            aspernatur quisquam eaque dolorem?
+          </p>
+          <img src={blogimg} alt="blog picture" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Explore;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "../../assets/globals.css";
+import heroimg from "../../assets/home1.jpg";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-main">
+      <div className="home-hero">
+        <img src={heroimg} alt="hero image" />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
