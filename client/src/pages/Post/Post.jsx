fd864618602586fc0748f68499555d022f5bbb73
IKANYI <paulkaranja214@gmail.com>
2024-07-20 12:16:26 +0300

    create post page

--- client/src/pages/Post/Post.jsx
+++ client/src/pages/Post/Post.jsx
@@ -1,7 +1,48 @@
+import { useFormik } from "formik";
 import React from "react";
+import "../../assets/globals.css";
 
 function Post() {
-  return <div>Post</div>;
+  const formik = useFormik({
+    initialValues: {
+      postMedia: "",
+      postText: "",
+    },
+    onSubmit: (formState) => {
+      console.log({
+        ...formState,
+        postMedia: formState.postMedia.name,
+      });
+    },
+  });
+  return (
+    <div className="post-main">
+      <div className="post-content">
+        <form onSubmit={formik.handleSubmit} className="posts-form">
+          <input
+            type="file"
+            name="postMedia"
+            onChange={(event) =>
+              formik.setFieldValue("postMedia", event.currentTarget.files[0])
+            }
+            onBlur={formik.handleBlur}
+          />
+          <input
+            type="text"
+            name="postText"
+            placeholder="Type your post here"
+            className="post-text"
+            value={formik.values.postText}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <button type="submit" className="post-button">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
